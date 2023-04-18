9c11505d0621ffae9ba2fd3a55feaf7459fe7f7a
amisha-w <amishabw@gmail.com>
2023-04-18 18:20:56 -0400

    Added base files

---
+++ src/utils.py
@@ -0,0 +1,245 @@
+import sys, re, math, copy, json, sys
+sys.path.append("./src")
+from constants import *
+from pathlib import Path
+from sym import Sym
+from operator import itemgetter
+
+def diffs(nums1,nums2):
+  def fun(k,nums):
+        return cliffsDelta(nums.has,nums2[k].has),nums.txt
+  return kap(nums1, fun)
+
+def cliffsDelta(ns1,ns2):
+    if len(ns1) > 256:
+        ns1 = many(ns1,256)
+    if len(ns2) > 256:
+        ns2 = many(ns2,256)
+    if len(ns1) > 10*len(ns2):
+        ns1 = many(ns1,10*len(ns2))
+    if len(ns2) > 10*len(ns1):
+        ns2 = many(ns2,10*len(ns1))
+    n,gt,lt = 0,0,0
+    for x in ns1:
+        for y in ns2:
+            n = n + 1
+            if x > y:
+                gt = gt + 1
+            if x < y:
+                lt = lt + 1
+    return abs(lt - gt)/n > options['cliffs']
+
+def showTree(node, what, cols, nPlaces, lvl = 0):
+  if node:
+    print('|.. ' * lvl + '[' + str(len(node['data'].rows)) + ']' + '  ', end = '')
+    if not node.get('left') or lvl==0:
+        print(node['data'].stats("mid",node['data'].cols.y,nPlaces))
+    else:
+        print('')
+    showTree(node.get('left'), what,cols, nPlaces, lvl+1)
+    showTree(node.get('right'), what,cols,nPlaces, lvl+1)
+
+def bins(cols,rowss):
+    out = []
+    for col in cols:
+        ranges = {}
+        for y,rows in rowss.items():
+            for row in rows:
+                x = row.cells[col.at]
+                if x != "?":
+                    k = int(bin(col,x))
+                    if not k in ranges:
+                        ranges[k] = range_fun(col.at,col.txt,x)
+                    extend(ranges[k], x, y)
+        ranges = list(dict(sorted(ranges.items())).values())
+        r = ranges if isinstance(col, Sym) else mergeAny(ranges)
+        out.append(r)
+    return out
+
+def bin(col,x):
+    if x=="?" or isinstance(col, Sym):
+        return x
+    tmp = (col.hi - col.lo)/(options['bins'] - 1)
+    return  1 if col.hi == col.lo else math.floor(x/tmp + .5)*tmp
+
+def coerce(s):
+    if s == 'true':
+        return True
+    elif s == 'false':
+        return False
+    elif s.isdigit():
+        return int(s)
+    elif '.' in s and s.replace('.', '').isdigit():
+        return float(s)
+    else:
+        return s
+
+def eg(key, str, fun):
+    egs[key] = fun
+    global help
+    help = help + '  -g '+ key + '\t' + str + '\n'
+
+def rint(lo,hi, mSeed = None):
+    return math.floor(0.5 + rand(lo,hi, mSeed))
+
+
+def rand(lo, hi, mSeed = None):
+    lo, hi = lo or 0, hi or 1
+    global Seed
+    Seed = 1 if mSeed else (16807 * Seed) % 2147483647
+    return lo + (hi-lo) * Seed / 2147483647
+
+def rnd(n, nPlaces = 3):
+    mult = 10**nPlaces
+    return math.floor(n * mult + 0.5) / mult
+
+def csv(file, fun):
+    t = []
+    with open(file, 'r', encoding='utf-8') as file:
+        for _, line in enumerate(file):
+            row = list(map(coerce, line.strip().split(',')))
+            t.append(row)
+            fun(row)
+
+def kap(t, fun):
+    u = {}
+    for v in t:
+        k = t.index(v)
+        v, k = fun(k,v)
+        u[k or len(u)] = v
+    return u
+
+def dict_kap(t, fun):
+    u = {}
+    for k,v in t.items():
+        v, k = fun(k,v)
+        u[k or len(u)] = v
+    return u
+
+def cosine(a,b,c):
+    den = 1 if c == 0 else 2*c
+    x1 = (a**2 + c**2 - b**2) / den
+    x2 = max(0, min(1, x1))
+    y  = abs((a**2 - x2**2))**.5
+    if isinstance(y, complex):
+        print('a', a)
+        print('x1', x1)
+        print('x2', x2)
+    return x2, y
+
+def any(t):
+    return t[rint(0, len(t) - 1)]
+
+def many(t, n):
+    arr = []
+    for index in range(1, n + 1):
+        arr.append(any(t))
+    return arr
+
+def show(node, what, cols, nPlaces, lvl = 0):
+  if node:
+    print('|..' * lvl, end = '')
+    if not node.get('left'):
+        print(node['data'].rows[-1].cells[-1])
+    else:
+        print(int(rnd(100*node['c'], 0)))
+    show(node.get('left'), what,cols, nPlaces, lvl+1)
+    show(node.get('right'), what,cols,nPlaces, lvl+1)
+
+def deepcopy(t):
+    return copy.deepcopy(t)
+
+def oo(t):
+    d = t.__dict__
+    d['a'] = t.__class__.__name__
+    d['id'] = id(t)
+    d = dict(sorted(d.items()))
+    print(d)
+
+def merge(col1,col2):
+  copied = deepcopy(col1)
+  if isinstance(col1, Sym):
+      for n in col2.has:
+        copied.add(n)
+  else:
+    for n in col2.has:
+        copied.add(new,n)
+    copied.lo = min(col1.lo, col2.lo)
+    copied.hi = max(col1.hi, col2.hi)
+  return copied
+
+def range_fun(at,txt,lo,hi=None):
+    return {'at':at,'txt':txt,'lo':lo,'hi':lo or hi or lo,'y':Sym()}
+
+def extend(range,n,s):
+    range['lo'] = min(n, range['lo'])
+    range['hi'] = max(n, range['hi'])
+    range['y'].add(s)
+
+def value(has,nB = None, nR = None, sGoal = None):
+    sGoal,nB,nR = sGoal or True, nB or 1, nR or 1
+    b,r = 0,0
+    for x,n in has.items():
+        if x==sGoal:
+            b = b + n
+        else:
+            r = r + n
+    b,r = b/(nB+1/float("inf")), r/(nR+1/float("inf"))
+    return b**2/(b+r)
+
+
+def merge2(col1,col2):
+  new = merge(col1,col2)
+  if new.div() <= (col1.div()*col1.n + col2.div()*col2.n)/new.n:
+    return new
+
+def mergeAny(ranges0):
+    def noGaps(t):
+        for j in range(1,len(t)):
+            t[j]['lo'] = t[j-1]['hi']
+        t[0]['lo']  = float("-inf")
+        t[len(t)-1]['hi'] =  float("inf")
+        return t
+
+    ranges1,j = [],0
+    while j <= len(ranges0)-1:
+        left = ranges0[j]
+        right = None if j == len(ranges0)-1 else ranges0[j+1]
+        if right:
+            y = merge2(left['y'], right['y'])
+            if y:
+                j = j+1
+                left['hi'], left['y'] = right['hi'], y
+        ranges1.append(left)
+        j = j+1
+    return noGaps(ranges0) if len(ranges0)==len(ranges1) else mergeAny(ranges1)
+
+def firstN(sortedRanges,scoreFun):
+    print("")
+    def function(r):
+        print(r['range']['txt'],r['range']['lo'],r['range']['hi'],rnd(r['val']),r['range']['y'].has)
+    _ = list(map(function, sortedRanges))
+    print()
+    first = sortedRanges[0]['val']
+    def useful(range):
+        if range['val']>.05 and range['val']> first/10:
+            return range
+    sortedRanges = [x for x in sortedRanges if useful(x)]
+    most,out = -1, -1
+    for n in range(1,len(sortedRanges)+1):
+        temp = sortedRanges[0:n]
+        t_range = [x['range'] for x in temp]
+        tmp,rule = scoreFun(t_range)
+        if tmp and tmp > most:
+            out,most = rule,tmp
+    return out,most
+
+def prune(rule, maxSize):
+    n=0
+    for txt,ranges in rule.items():
+        n = n+1
+        if len(ranges) == maxSize[txt]:
+            n=n+1
+            rule[txt] = None
+    if n > 0:
+        return rule
